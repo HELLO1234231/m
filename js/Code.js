083d1631a082b65f8ab6feeb7f220cf5400b8277
HELLO1234231 <94646830+HELLO1234231@users.noreply.github.com>
2022-02-12 13:36:23 +0000

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,32 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+const accessCode4 = "Html And Css And Js Are All Coding Languages";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 100,250)
+    fill("lightblue")
+    text("Hint: Stores all information !!", 100,270)
+
+    fill("white")
+    textSize(15)
+    text("C S S  C O D I N G  L A N G U A G E S  J S  H T M L  A R E  A L L  A N D , ", 300,400) 
+    fill("lightblue")
+    text("Hint: What are JS,HTML and CSS " , 300,420)
+    
+}
